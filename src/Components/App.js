950702b0b3b6a8ff5cbfc4b52720aef66c4328cb
이다은 <daeun.lee@coinplug.com>
2019-08-09 15:22:24 +0900

    login 화면 구현

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 // import { gql } from "apollo-boost";
-import { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from '../Styles/Theme';
 import Router from './Router';
 // import { useQuery } from "react-apollo-hooks";
 import { defaults } from "../Apollo/LocalState";
+import Footer from "./Footer";
 
 // const QUERY = gql`{
 //     isLoggedIn @client
 // }`;
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: ${props => props.theme.maxWidth};
+  width: 100%;
+`;
 
 export default () => {
   // const { data: { isLoggedIn } } = useQuery(QUERY);
@@ -20,7 +27,10 @@ export default () => {
   <ThemeProvider theme={Theme}>
     <>
       <GlobalStyles />
-      <Router isLoggedIn={defaults.isLoggedIn}/>
+      <Wrapper>
+        <Router isLoggedIn={defaults.isLoggedIn}/>
+        <Footer />
+      </Wrapper>
     </>
   </ThemeProvider>
   );
