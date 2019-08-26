b461401adbbaa92d58ff5f4d08c82fd182451110
이다은 <daeun.lee@coinplug.com>
2019-08-26 16:42:11 +0900

    메뉴 구현

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -12,8 +12,8 @@ import styled from "styled-components";
     padding: 0px 15px;
 `;
 
- const Input = ({ placeholder, required = true, value, onChange, type="text"}) => (
-    <Container placeholder={placeholder} required={required} value={value} onChange={onChange} type={type}/>
+ const Input = ({ placeholder, required = true, value, onChange, type="text", className}) => (
+    <Container className={className} placeholder={placeholder} required={required} value={value} onChange={onChange} type={type}/>
   );
 
  Input.propTypes = {
